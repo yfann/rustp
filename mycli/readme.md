caaaaf90c928bf7d395838e8ea405fde74db3c1c
fan <fan.yu@philips.com>
2024-12-05 16:54:20 +0800

    update

--- mycli/readme.md
+++ mycli/readme.md
@@ -1,5 +1,9 @@
 
 
-## windows 
+## powershell 
 + `cargo run --bin catr -- -n (Get-ChildItem .\test\*.txt)`
+
+
+## cmd
 + `cargo run --bin catr -- ./test/a.txt -n`
++ `cargo run --bin headr -- -n 3 test/a.txt`
---
+++ mycli/src/bin/headr.rs
@@ -0,0 +1,66 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Parser,Debug)]
+#[command(author, version, about)]
+struct Args{
+    #[arg(default_value="-",value_name="FILE")]
+    files: Vec<String>,
+    #[arg(short('n'),long,default_value="10",value_name="LINES",value_parser=clap::value_parser!(u64).range(1..))]
+    lines: u64,
+    #[arg(short('c'),long,value_name="BYTES",conflicts_with("lines"),value_parser=clap::value_parser!(u64).range(1..))]
+    bytes: Option<u64>,
+}
+fn main(){
+    if let Err(e)=run(Args::parse()){
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<(),Box<dyn std::error::Error>> {
+    let num_files = args.files.len();
+
+    for (file_num, filename) in args.files.iter().enumerate() {
+        match open(filename) {
+            Err(err) => eprintln!("{filename}: {err}"),
+            Ok(mut file) => {
+                if num_files > 1 {
+                    println!(
+                        "{}==> {filename} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                    );
+                }
+
+                if let Some(num_bytes) = args.bytes {
+                    let mut buffer = vec![0; num_bytes as usize];
+                    let bytes_read = file.read(&mut buffer)?;
+                    print!(
+                        "{}",
+                        String::from_utf8_lossy(&buffer[..bytes_read])
+                    );
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..args.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{line}");
+                        line.clear();
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>,Box<dyn std::error::Error>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
